0a3eca6291b5b7ea104c10c9b854f42b278a30ce
hoangeel <lvhoangbagia@gmail.com>
2022-06-04 13:46:16 +0700

    first commit

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,47 +1,46 @@
 import React from 'react'
 import "./index.scss"
 import LOGO from "../../assets/images/LOGO.png"
+import Telegram from "../../assets/images/item_Telegram.png"
+import Twitter from "../../assets/images/item_Twitter_black.png"
+import Medium from "../../assets/images/item_Medium.png"
 
 export default function Footer() {
   return (
     <div className='footer'>
         <div className='contentFooter'>
-            <ul className="nav">
-                <li className="nav-item item_LOGO">
-                    <img src={LOGO} alt="LOGO" />
-                    <p className="nav-link active">
-                        Sandwich Network is the definite hub to start your decentralized cryptocurrency journey. Join us!
-                    </p>
-                </li>
-                <li className="nav-item Products">
-                    <h4>Products</h4>
-                    <p className="nav-link">
-                        SandwichBeta
-                        SandwichSAFU
-                        SandwichTools
-                        SandwichSwap
-                    </p>
-                </li>
-                <li className="nav-item Support">
-                    <h4>Support</h4>
-                    <p className="nav-link">
-                        Tutorials
-                        Documentation
-                        Customer support
-                    </p>
-                </li>
-                <li className="nav-item Social">
-                    <h4>Social</h4>
-                    <p className="nav-link">
-                        Telegram
-                        Twitter
-                        Medium
-                    </p>
-                    <div className='item_Telegram'></div>
-                    <div className='item_Twitter_black'></div>
-                    <div className='item_Medium'></div>
-                </li>
-            </ul>
+            <div className='logo'>
+                <img src={LOGO} alt="logo" />
+                <p className=''>Sandwich Network is the definite hub to start your decentralized cryptocurrency journey. Join us!</p>
+            </div>
+            <div className='Products'>
+                <h4>Products</h4>
+                <p>SandwichBeta</p>
+                <p>SandwichSAFU</p>
+                <p>SandwichTools</p>
+                <p>SandwichSwap</p>
+            </div> 
+            <div className='Support'>
+                <h4>Support</h4>
+                <p>Tutorials</p>
+                <p>Documentation</p>
+                <p>Customer support</p>
+            </div> 
+            <div className='Social'>
+                <h4>Social</h4>
+                <div className='Telegram'>
+                    <img src={Telegram} alt="" />
+                    <p>Telegram</p>
+                </div>
+                <div className='Twitter'>
+                    <img src={Twitter} alt="" />
+                    <p>Twitter</p>
+                </div>
+                <div className='Medium'>
+                    <img src={Medium} alt="" />
+                    <p>Medium</p>
+                </div>
+            </div>                
         </div>
         <div className='end'>Copyright © 2021 Sandwich Network. All rights reserved.</div>
     </div>
